14238b8134220aa35c00cfc98e917556f1ababd7
Hafsi <300151732@monboreal.ca>
2026-04-18 23:41:26 -0400

    Creation de la classe ViewSalle

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ salle = Salle("S101","Salle équipée d’un projecteur.","Informatique",25)
 salle2 = Salle("S104","Salle  table centrale et écran.","Réunion",20)
 salle3= Salle("S103","Salle polyvalente adaptée aux formations.","Formation",0)
 
-service = ServiceSalle()
-service.recuperer_salle()
+data= DataSalle()
+data.add_data(salle2)
 
 
 
---
+++ views/view_salle.py
@@ -0,0 +1,8 @@
+from services.services_salle import ServiceSalle
+
+
+
+class ViewSalle:
+    def __init__(self):
+        self.service_salle = ServiceSalle()
+
