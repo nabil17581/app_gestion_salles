c582d55495bae9edf75a9cf87f2cfd3fc1523c57
Hafsi <300151732@monboreal.ca>
2026-04-19 12:50:15 -0400

    Ajout de la methode get_info, modifier la methode add_salle et creation de la methode update_salle

--- views/view_salle.py
+++ views/view_salle.py
@@ -61,17 +61,34 @@ class ViewSalle:
         self.btn_rechercher = ctk.CTkButton(self.frame_action, text="Rechercher")
         self.btn_rechercher.grid(row=0, column=3, padx=10, pady=10)
 
-    #Ajouter une salle
-    def ajout_salle(self):
+    def get_info(self):
         code = self.entry_code.get()
         description = self.entry_desc.get()
-        cat = self.entry_cat.get()
-        cap = self.entry_cap.get()
+        categorie = self.entry_cat.get()
+        capacite = self.entry_cap.get()
 
-        salle = Salle(code, description, cat, cap)
+        salle = Salle(code, description, categorie, capacite)
+        return salle
 
-        msg = self.service_salle.ajouter_salle(salle)
+
+
+
+
+
+    #Ajouter une salle
+    def ajout_salle(self):
+
+        salle_add = self.get_info()
+
+        msg = self.service_salle.ajouter_salle(salle_add)
         print(msg)
+
+    #Modifier salle
+    def update_salle(self):
+        salle_update = self.get_info()
+        msg = self.service_salle.modifier_salle(salle_update)
+        print(msg)
+
 
 
     def run(self):
